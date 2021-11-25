485cef9a796aaccd4685f5a24f601524f784b22f
Stephen Williams <82328038+stephen-gcp1@users.noreply.github.com>
2021-11-25 07:44:03 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>AirTwig CMS </title>
+
+    <!-- Bootstrap -->
+    <link href="vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- Animate.css -->
+    <link href="vendors/animate.css/animate.min.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="build/css/custom.min.css" rel="stylesheet">
+  </head>
+
+  <body class="login">
+    <div>
+      <a class="hiddenanchor" id="signup"></a>
+      <a class="hiddenanchor" id="signin"></a>
+
+      <div class="login_wrapper">
+        <div class="animate form login_form">
+          <section class="login_content">
+            <form action="production/authenticate.php" method="post">
+              <h6>AirTwig Login</h6>
+              <div>
+                <input type="text" class="form-control" name="username" placeholder="Username" required="" />
+              </div>
+              <div>
+                <input type="password" class="form-control" name="password"  placeholder="Password" required="" />
+              </div>
+              <div>
+              <input class="btn btn-primary btn-sm" type="submit" value="Login">
+                <a class="reset_pass" href="#">Lost your password?</a>
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+                <p class="change_link">New to site?
+                  <a href="#signup" class="to_register"> Create Account </a>
+                </p>
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h6><i class="fa fa-bank"></i> AirTwig</h6>
+                  <p>©2021 All Rights Reserved. AirTwig</p>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+
+        <div id="register" class="animate form registration_form">
+          <section class="login_content">
+            <form  method="post" action="register.php">
+              <h6>AirTwig Register</h6>
+              <div>
+                <input type="text" class="form-control" name="username" placeholder="Username" required="" />
+              </div>
+              <div>
+                <input type="email" class="form-control" name="email" placeholder="Email" required="" />
+              </div>
+              <div>
+                <input type="password" class="form-control" name="password" placeholder="Password" required="" />
+              </div>
+              <div>
+                <input class="btn btn-success btn-sm" type="submit" value="Register">
+              </div>
+
+              <div class="clearfix"></div>
+
+              <div class="separator">
+                <p class="change_link">Already a member ?
+                  <a href="#signin" class="to_register"> Log in </a>
+                </p>
+
+                <div class="clearfix"></div>
+                <br />
+
+                <div>
+                  <h6><i class="fa fa-bank"></i> AirTwig</h6>
+                  <p>©2021 All Rights Reserved. AirTwig</p>
+                </div>
+              </div>
+            </form>
+          </section>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
